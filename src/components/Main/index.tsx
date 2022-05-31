0be842b862ddef7cf6d15aeb11a235d048bed262
seohyunsim <ssh4563@naver.com>
2022-05-31 18:50:29 +0900

    메인 컴포 생성 및 레이아웃 구성

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,65 @@
-import React from "react";
 import styled from "styled-components";
+import { CgArrowLeftR, CgArrowRightR } from "react-icons/cg";
+import { TbArrowBigLeft, TbArrowBigRight } from "react-icons/tb";
 
 export const Main = () => {
-  return <Wrap>index</Wrap>;
+  const ImageUrl =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAIaro9NdPhttKXZ12oRy7smHDKl_bULANjA&usqp=CAU";
+  return (
+    <Wrap>
+      <h2>kitty collector</h2>
+      <Score>
+        <div className="scoreNum">0</div>
+        <span>score</span>
+      </Score>
+      <KittyGroup>
+        <Kitty src={ImageUrl} />
+        <Kitty src={ImageUrl} />
+        <Kitty src={ImageUrl} />
+        <Kitty src={ImageUrl} />
+        <Kitty src={ImageUrl} />
+        <Kitty src={ImageUrl} />
+      </KittyGroup>
+      <ArrowWrap>
+        <TbArrowBigLeft />
+        <TbArrowBigRight />
+      </ArrowWrap>
+    </Wrap>
+  );
 };
 
 const Wrap = styled.div`
-  border: 1px solid;
+  text-align: center;
+`;
+
+const Score = styled.div`
+  display: flex;
+  flex-direction: column;
+  .scoreNum {
+    font-size: 60px;
+  }
+  span {
+    font-size: 30px;
+  }
+`;
+
+const KittyGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 3em;
+`;
+
+const Kitty = styled.img`
+  width: 60px;
+  margin-top: -1em;
+`;
+
+const ArrowWrap = styled.div`
+  width: 100%;
+  font-size: 40px;
+  display: flex;
+  justify-content: space-around;
+  position: absolute;
+  top: 80%;
 `;
