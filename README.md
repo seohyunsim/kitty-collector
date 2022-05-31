ebd7cccfce96e7402e6c247774ee5a69d13720f2
seohyunsim <ssh4563@naver.com>
2022-05-31 18:10:42 +0900

    개발환경 세팅

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kitty-collector
+
 화살표 방향에 맞는 고양이 선별 게임
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Main = () => {
+  return <Wrap>index</Wrap>;
+};
+
+const Wrap = styled.div`
+  border: 1px solid;
+`;
