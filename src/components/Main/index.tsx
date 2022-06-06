a2264671f6537547199c9d567aa0abeb22e14b06
seohyunsim <ssh4563@naver.com>
2022-06-06 18:46:26 +0900

    kitty state 지정 및 이벤트 생성

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,27 +1,40 @@
 import styled, { keyframes, css } from "styled-components";
 import { TbArrowBigLeft, TbArrowBigRight } from "react-icons/tb";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Main = () => {
-  const [score, setScore] = useState<number>(0);
-
   const blackKitty =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAIaro9NdPhttKXZ12oRy7smHDKl_bULANjA&usqp=CAU";
 
   const whiteKitty =
     "https://cdn.pixabay.com/photo/2013/07/12/15/40/cat-150306_960_720.png";
 
-  const kittys: string[] = [
+  const [score, setScore] = useState<number>(0);
+  const [kittys, setKittys] = useState<string[]>([
     blackKitty,
     whiteKitty,
     whiteKitty,
     blackKitty,
     whiteKitty,
-    blackKitty,
-  ];
+  ]);
 
-  const onLeftArrowClick = () => {
-    setScore(score + 1);
+  const kittysList: React.ReactNode = kittys.reverse().map((url, idx) => {
+    return <Kitty key={idx} src={url} />;
+  });
+
+  console.log(kittys);
+
+  const onLeftArrowClick = (e: React.MouseEvent<SVGElement>) => {
+    const kittyId = (e.target as SVGElement).id;
+    if (kittyId === kittys[4]) {
+      console.log("똑같음!", kittyId);
+      kittys.splice(4, 1);
+      setKittys(kittys);
+      return kittys;
+    } else {
+      console.log("다름~", kittyId, kittys[4]);
+    }
+    console.log(kittys);
     return rotateKitty;
   };
 
@@ -45,16 +58,14 @@ export const Main = () => {
         <span>score</span>
       </Score>
       <KittyGroup>
-        <div className="Kittys">
-          {kittys.reverse().map((url, idx) => {
-            return <Kitty key={idx} src={url} />;
-          })}
-        </div>
+        <div className="Kittys">{kittysList}</div>
       </KittyGroup>
       <ArrowWrap>
         <LeftArrow>
           <img src={blackKitty} />
-          <TbArrowBigLeft onClick={onLeftArrowClick} />
+          <TbArrowBigLeft onClick={onLeftArrowClick} id={blackKitty}>
+            {blackKitty}
+          </TbArrowBigLeft>
         </LeftArrow>
         <RightArrow>
           <img src={whiteKitty} />
