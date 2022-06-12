10df7c7432d9c2691a3a33d7a0ca62b5942882d3
seohyunsim <ssh4563@naver.com>
2022-06-12 23:29:49 +0900

    css 개선 및 조건부 css 테스트

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,15 @@
 import styled, { keyframes } from "styled-components";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Main = () => {
   const greyKitty =
     "https://deciduous-petalite-828.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fbc1ba5b8-4dc2-40f8-8e20-61152e968c23%2F22.png?table=block&id=6fae8800-4fd0-4e00-8ac5-4a5c8044c3e9&spaceId=757517fd-f82c-4a1d-8b05-6567709f4aa0&width=2000&userId=&cache=v2";
   const brownKitty =
     "https://deciduous-petalite-828.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fc34409a7-0239-4aa2-97d5-60ded89645f7%2F11.png?table=block&id=b2b0aa9f-4fe4-479a-8481-e5604c55d7cd&spaceId=757517fd-f82c-4a1d-8b05-6567709f4aa0&width=2000&userId=&cache=v2";
+  const leftArrow =
+    "https://deciduous-petalite-828.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F9e83368a-70ea-420e-ade2-01056a7d1831%2Fleft.png?table=block&id=f4616461-b0dc-4967-954b-506da84b6ef6&spaceId=757517fd-f82c-4a1d-8b05-6567709f4aa0&width=1020&userId=&cache=v2";
+  const rightArrow =
+    "https://deciduous-petalite-828.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F04ed97e2-12c3-4629-89f3-06b8db004bbf%2Fnext.png?table=block&id=f1f3e603-0035-4f4b-918f-4f423b26c322&spaceId=757517fd-f82c-4a1d-8b05-6567709f4aa0&width=1020&userId=&cache=v2";
 
   const randomKitty = [greyKitty, brownKitty];
   const [score, setScore] = useState<number>(0);
@@ -43,7 +47,9 @@ export const Main = () => {
         <h2>kitty collector</h2>
       </div>
       <Score>
-        <div className="scoreNum">{score}</div>
+        <ScoreNum scoreColor scoreSize>
+          {score}
+        </ScoreNum>
         <span>score</span>
       </Score>
       <KittyGroup>
@@ -60,7 +66,7 @@ export const Main = () => {
             alt="leftArrow"
             onClick={onArrowClick}
             id={greyKitty}
-            src="https://deciduous-petalite-828.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F9e83368a-70ea-420e-ade2-01056a7d1831%2Fleft.png?table=block&id=f4616461-b0dc-4967-954b-506da84b6ef6&spaceId=757517fd-f82c-4a1d-8b05-6567709f4aa0&width=1020&userId=&cache=v2"
+            src={leftArrow}
           />
         </LeftArrow>
         <RightArrow>
@@ -69,7 +75,7 @@ export const Main = () => {
             alt="rightArrow"
             onClick={onArrowClick}
             id={brownKitty}
-            src="https://deciduous-petalite-828.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F04ed97e2-12c3-4629-89f3-06b8db004bbf%2Fnext.png?table=block&id=f1f3e603-0035-4f4b-918f-4f423b26c322&spaceId=757517fd-f82c-4a1d-8b05-6567709f4aa0&width=1020&userId=&cache=v2"
+            src={rightArrow}
           />
         </RightArrow>
       </ArrowWrap>
@@ -95,21 +101,31 @@ const Score = styled.div`
   padding: 1em;
   display: flex;
   flex-direction: column;
-  .scoreNum {
-    @font-face {
-      font-family: "SDSamliphopangche_Outline";
-      src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts-20-12@1.0/SDSamliphopangche_Outline.woff")
-        format("woff");
-      font-weight: normal;
-      font-style: normal;
-    }
-    font-size: 85px;
-    font-family: "SDSamliphopangche_Outline";
-    color: #3e3e3e;
-  }
   span {
     font-size: 20px;
     padding: 5px;
+  }
+`;
+
+const ScoreNum = styled.div<{
+  scoreColor?: boolean;
+  scoreSize?: boolean;
+}>`
+  width: 80%;
+  margin: 0 auto;
+  @font-face {
+    font-family: "SDSamliphopangche_Outline";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts-20-12@1.0/SDSamliphopangche_Outline.woff")
+      format("woff");
+    font-weight: normal;
+    font-style: normal;
+  }
+  font-size: 85px;
+  font-family: "SDSamliphopangche_Outline";
+  color: ${(props) => (props.scoreColor ? "#353535" : "#cd0000")};
+  &:hover {
+    transform: scale(110%);
+    transition: 0.3s;
   }
 `;
 
@@ -143,8 +159,6 @@ const ArrowWrap = styled.div`
   font-size: 40px;
   display: flex;
   justify-content: space-around;
-  position: absolute;
-  top: 75%;
 `;
 
 const LeftArrow = styled.div`
@@ -152,6 +166,9 @@ const LeftArrow = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 10px;
+  position: absolute;
+  top: 75%;
+  right: 75%;
   img {
     width: 60px;
   }
@@ -162,6 +179,9 @@ const RightArrow = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 10px;
+  position: absolute;
+  top: 75%;
+  left: 75%;
   img {
     width: 60px;
   }
