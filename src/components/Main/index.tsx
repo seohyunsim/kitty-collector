d16a5aad3e5578f67099b06cb82cd5758bf3d542
seohyunsim <ssh4563@naver.com>
2022-06-17 23:23:13 +0900

    function description import

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -35,6 +35,7 @@ export const Main = () => {
     setScore(score + 1);
   };
 
+  //화살표 키보드 이벤트
   const onKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
     const keyboard = e.key;
 
@@ -47,6 +48,7 @@ export const Main = () => {
     }
   };
 
+  //화살표 클릭 이벤트
   const onArrowClick = (e: React.MouseEvent<HTMLInputElement>) => {
     e.preventDefault();
     const kittyId = (e.target as HTMLImageElement).id;
