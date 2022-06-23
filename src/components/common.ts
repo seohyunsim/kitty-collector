0b2f7cee5245b771ccbd56c789866a58c3dfc8a7
seohyunsim <ssh4563@naver.com>
2022-06-23 23:11:32 +0900

    src 변수 파일 분리 및 common 파일생성

---
+++ src/components/common.ts
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const greyKitty =
+  "https://deciduous-petalite-828.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fbc1ba5b8-4dc2-40f8-8e20-61152e968c23%2F22.png?table=block&id=6fae8800-4fd0-4e00-8ac5-4a5c8044c3e9&spaceId=757517fd-f82c-4a1d-8b05-6567709f4aa0&width=2000&userId=&cache=v2";
+export const brownKitty =
+  "https://deciduous-petalite-828.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fc34409a7-0239-4aa2-97d5-60ded89645f7%2F11.png?table=block&id=b2b0aa9f-4fe4-479a-8481-e5604c55d7cd&spaceId=757517fd-f82c-4a1d-8b05-6567709f4aa0&width=2000&userId=&cache=v2";
+export const leftArrow =
+  "https://deciduous-petalite-828.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F9e83368a-70ea-420e-ade2-01056a7d1831%2Fleft.png?table=block&id=f4616461-b0dc-4967-954b-506da84b6ef6&spaceId=757517fd-f82c-4a1d-8b05-6567709f4aa0&width=1020&userId=&cache=v2";
+export const rightArrow =
+  "https://deciduous-petalite-828.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F04ed97e2-12c3-4629-89f3-06b8db004bbf%2Fnext.png?table=block&id=f1f3e603-0035-4f4b-918f-4f423b26c322&spaceId=757517fd-f82c-4a1d-8b05-6567709f4aa0&width=1020&userId=&cache=v2";
